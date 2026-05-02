d0eab81d8ffb285e45b81d013f3c0593aadc4ad3
cjkimber <cjkimber@gmail.com>
2026-05-02 23:29:57 +0100

    Create switchoff.js

---
+++ pages/api/switchoff.js
@@ -0,0 +1,33 @@
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('productivity');
+  const collection = db.collection('switchoff');
+
+  if (req.method === 'GET') {
+    const { month, year } = req.query;
+    const query = {};
+    if (month && year) {
+      query.date = { $regex: `^${year}-${String(month).padStart(2, '0')}` };
+    }
+    const entries = await collection.find(query).toArray();
+    return res.status(200).json(entries);
+  }
+
+  if (req.method === 'POST') {
+    const { date, time } = req.body;
+    await collection.deleteOne({ date });
+    const entry = { date, time };
+    await collection.insertOne(entry);
+    return res.status(201).json(entry);
+  }
+
+  if (req.method === 'DELETE') {
+    const { date } = req.body;
+    await collection.deleteOne({ date });
+    return res.status(200).json({ deleted: true });
+  }
+
+  res.status(405).end();
+}
